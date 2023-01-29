d370dbe3d4937eb0fbe06a8c7592b806558942d9
Simone Basile <simonebasile199@gmail.com>
2023-01-29 21:22:39 +0100

    Added alert function

--- assets/js/index.js
+++ assets/js/index.js
@@ -151,12 +151,13 @@ const rankedTitles = () => {
     return a.rank - b.rank;
   });
 
-  /*   let alert = document.querySelector(".alert");
+  let alert = document.querySelector(".alert");
   alert.innerHTML = "";
   sortedSongs.forEach((song) => {
     alert.innerHTML += `<li class="list-group-item">
     ${song.title} - ${song.rank}`;
-  }); */
+  });
+  return alert;
 };
 
 window.onload = favSongs(favSongsUrl);
@@ -164,3 +165,81 @@ window.onload = drawFavSong();
 for (let i = 0; i < albumsUrl.length; i++) {
   getAlbums(albumsUrl[i]);
 }
+
+/* function CustomAlert() {
+  this.alert = function (message) {
+    document.body.innerHTML =
+      document.body.innerHTML +
+      `<div id="dialogoverlay"></div>
+        <div id="dialogbox" class="slit-in-vertical">
+          <div>
+            <div id="dialogboxbody"></div>
+            <div id="dialogboxfoot"></div>
+          </div>
+        </div>`;
+
+    let dialogoverlay = document.getElementById("dialogoverlay");
+    let dialogbox = document.getElementById("dialogbox");
+
+    let winH = window.innerHeight;
+    dialogoverlay.style.height = winH + "px";
+
+    dialogbox.style.top = "100px";
+
+    dialogoverlay.style.display = "block";
+    dialogbox.style.display = "block";
+
+    document.getElementById("dialogboxbody").innerHTML = message;
+    document.getElementById("dialogboxfoot").innerHTML =
+      '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+  };
+
+  this.ok = function () {
+    document.getElementById("dialogbox").style.display = "none";
+    document.getElementById("dialogoverlay").style.display = "none";
+  };
+} */
+
+function CustomAlert() {
+  this.alert = function (message, title) {
+    document.body.innerHTML =
+      document.body.innerHTML +
+      '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+
+    let dialogoverlay = document.getElementById("dialogoverlay");
+    let dialogbox = document.getElementById("dialogbox");
+
+    let winH = window.innerHeight;
+    dialogoverlay.style.height = winH + "px";
+
+    dialogbox.style.top = "100px";
+
+    dialogoverlay.style.display = "block";
+    dialogbox.style.display = "block";
+
+    document.getElementById("dialogboxhead").style.display = "block";
+
+    if (typeof title === "undefined") {
+      document.getElementById("dialogboxhead").style.display = "none";
+    } else {
+      document.getElementById("dialogboxhead").innerHTML =
+        '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title;
+    }
+    document.getElementById("dialogboxbody").innerHTML = message;
+    document.getElementById("dialogboxfoot").innerHTML =
+      '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+  };
+
+  this.ok = function () {
+    document.getElementById("dialogbox").style.display = "none";
+    document.getElementById("dialogoverlay").style.display = "none";
+  };
+}
+
+let customAlert = new CustomAlert();
+
+const getAndPrintRanks = () => {
+  rankedTitles();
+  let alert = document.querySelector(".alert").innerHTML;
+  customAlert.alert(alert);
+};
